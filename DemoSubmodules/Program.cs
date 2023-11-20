4d07fe73941c0f2dd8527ec32d6ac5dc7e7c5ef8
DKriukov777 <DKriukov@eagle.org>
2023-11-20 13:13:33 -0600

    Added submodules

--- DemoSubmodules/Program.cs
+++ DemoSubmodules/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using SimpleCalculatorLib;
+
+var calculator = new DemoCalculator();
 Console.WriteLine("Hello, World!");
+Console.WriteLine(calculator.Minus(5,2));
